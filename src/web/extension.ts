5eba5f28e8ea2fe59706a9698ec3dad5a4a85e2b
Fabian Streicher <fabi.streicher@gmail.com>
2022-08-07 09:54:42 +0200

    fix: create folding ranges for all decorators

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -3,24 +3,13 @@ import { DecoratorFoldingRangeProvider } from './foldingProvider';
 
 export function activate(context: ExtensionContext) {
 
-  context.subscriptions.push(
-    commands.registerCommand('folding-decorators.foldAll', () => {
-      window.showInformationMessage('Hello World from Folding Decorators in a web extension host!');
-    }),
-    commands.registerCommand('folding-decorators.unfoldAll', () => {
-      window.showInformationMessage('Hello World from Folding Decorators in a web extension host!');
-    }),
-    commands.registerCommand('folding-decorators.foldCustom', () => {
-      window.showInformationMessage('Hello World from Folding Decorators in a web extension host!');
-    }),
-    commands.registerCommand('folding-decorators.unfoldCustom', () => {
-      window.showInformationMessage('Hello World from Folding Decorators in a web extension host!');
-    })
-  );
-
   const decoratorFoldingRangeProvider = new DecoratorFoldingRangeProvider();
 
-  languages.registerFoldingRangeProvider('typescript', decoratorFoldingRangeProvider);
+  console.log('Activating...');
+  setTimeout(() => {
+    console.log('Registering Provider');
+    languages.registerFoldingRangeProvider('typescript', decoratorFoldingRangeProvider);
+  }, 3000);
 
 }
 
--- src/web/foldingProvider.ts
+++ src/web/foldingProvider.ts
@@ -3,8 +3,7 @@ import {
   FoldingRange,
   FoldingRangeKind,
   FoldingRangeProvider,
-  ProviderResult,
-  Range,
+  ProviderResult, 
   TextDocument
 } from 'vscode';
 
@@ -16,35 +15,56 @@ export class DecoratorFoldingRangeProvider implements FoldingRangeProvider {
 
   public provideFoldingRanges(document: TextDocument): ProviderResult<Array<FoldingRange>> {
     const ranges: Array<FoldingRange> = [];
+    const filteredRanges: Array<FoldingRange> = [];
 
     const text = document.getText();
-    const decoratorRegex = /(?<![_$[:alnum:]])(?:(?<=\.\.\.)|(?<!\.))\@\w+/g;
+    const decoratorRegex = /(?:^[ \t]*)(?:(?<=\.\.\.)|(?<!\.))@\w+.*/gm;
+    const customRegex = /^[ \t]*(?:(?<=\.\.\.)|(?<!\.))\@Api\w+/g;
+    const decoratedRegex = /(export)|(public)|(protected)|(private)/g;
+    const inlineDecoratorRegex = /(?:^[ \t]*)(?:(?<=\.\.\.)|(?<!\.))@\w+\(.*\).+,$/;
 
-    const matches: Array<RegExpMatchArray> = Array.from(text.matchAll(decoratorRegex));
+    const decoratorMatches: Array<RegExpMatchArray> = Array.from(text.matchAll(decoratorRegex));
+    const customMatches: Array<RegExpMatchArray> = Array.from(text.matchAll(customRegex));
+    const decoratedMatches: Array<RegExpMatchArray> = Array.from(text.matchAll(decoratedRegex));
 
-    for (const match of matches) {
-      const position = document.positionAt(match.index);
-      const range: Range = document.getWordRangeAtPosition(position);
+    for (const match of decoratorMatches) {
+
+      if (inlineDecoratorRegex.test(match[0])) {
+        console.log('Inline Decorator', match[0]);
+        continue;
+      }
+
+      const decoratorPosition = document.positionAt(match.index + match[0].indexOf('@'));
+      const decoratedMatch = decoratedMatches.find(m => m.index > match.index);
+      const decoratedPosition = document.positionAt(
+        decoratedMatch?.index ?? 0
+      );
       const foldingRange: FoldingRange = {
-        start: 174,
-        end: 245
+        start: decoratorPosition.line,
+        end: decoratedPosition.line - 1,
+        kind: FoldingRangeKind.Region
       };
 
-      // if (range) {
-      //   ranges.push(foldingRange);
-      // }
+      if (foldingRange.start !== foldingRange.end) {
+        ranges.push(foldingRange);
+      }
 
     }
+    ranges.sort(
+      (a, b) => {
+        if (a.end === b.end) {
+          return a.start - b.start;
+        }
+        return b.end - a.end;
+      });
 
-    const foldingRange: FoldingRange = {
-      start: 173,
-      end: 245,
-      kind: FoldingRangeKind.Region
-    };
+    ranges.forEach(r => {
+      if (!filteredRanges.some(fr => fr.end === r.end)) {
+        filteredRanges.push(r);
+      }
+    });
 
-    ranges.push(foldingRange);
-
-    return ranges;
+    return filteredRanges;
   }
 
 }
